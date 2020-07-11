0a5953af3e0df135ea38a3ae6321b67d9a6e768a
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-07-11 21:18:29 +0530

    stable session class

---
+++ build/session.d.ts
@@ -0,0 +1,66 @@
+import { AnyObject } from "@rheas/contracts";
+import { ISession } from "@rheas/contracts/sessions";
+export declare class Session implements ISession {
+    /**
+     * Complete session data.
+     *
+     * @var AnyObject
+     */
+    protected _data: AnyObject;
+    /**
+     * Last session accessed time in milliseconds.
+     *
+     * @var number
+     */
+    protected _lastAccessed: number;
+    /**
+     * Returns the last accessed time in milliseconds.
+     *
+     * @returns string
+     */
+    lastAccessed(): number;
+    /**
+     * Updates the last accessed time.
+     *
+     * @returns this
+     */
+    touch(): ISession;
+    /**
+     * Returns the session id.
+     *
+     * @params sessionId
+     *
+     * @returns
+     */
+    id(sessionId?: string): string;
+    /**
+     * Returns the session CSRF token.
+     *
+     * @params token
+     * @returns
+     */
+    csrf(token?: string): string;
+    /**
+     * Gets the session data for the key. If no value is found, defaultValue
+     * is returned.
+     *
+     * Supports deep get using dotted format.
+     *
+     * @param key
+     * @param defaultValue
+     */
+    get(key: string, defaultValue?: any): any;
+    /**
+     * Sets a new data on the session field.
+     *
+     * @param key
+     * @param value
+     */
+    set(key: string, value: any): ISession;
+    /**
+     * Returns the complete session data.
+     *
+     * @returns object
+     */
+    data(): AnyObject;
+}
---
+++ src/session.ts
@@ -0,0 +1,105 @@
+import { Obj } from "@rheas/support";
+import { AnyObject } from "@rheas/contracts";
+import { ISession } from "@rheas/contracts/sessions";
+
+export class Session implements ISession {
+
+    /**
+     * Complete session data.
+     * 
+     * @var AnyObject
+     */
+    protected _data: AnyObject = {};
+
+    /**
+     * Last session accessed time in milliseconds.
+     * 
+     * @var number
+     */
+    protected _lastAccessed: number = Date.now();
+
+    /**
+     * Returns the last accessed time in milliseconds.
+     * 
+     * @returns string
+     */
+    public lastAccessed(): number {
+        return this._lastAccessed;
+    }
+
+    /**
+     * Updates the last accessed time.
+     * 
+     * @returns this
+     */
+    public touch(): ISession {
+        this._lastAccessed = Date.now();
+
+        return this;
+    }
+
+    /**
+     * Returns the session id.
+     * 
+     * @params sessionId
+     * 
+     * @returns 
+     */
+    public id(sessionId: string = ""): string {
+        if (sessionId) {
+            this.set('id', sessionId);
+            return sessionId;
+        }
+        return this.get('id');
+    }
+
+    /**
+     * Returns the session CSRF token.
+     * 
+     * @params token
+     * @returns
+     */
+    public csrf(token: string = ""): string {
+        if (token) {
+            this.set('csrf', token);
+
+            return token;
+        }
+        return this.get('csrf');
+    }
+
+    /**
+     * Gets the session data for the key. If no value is found, defaultValue
+     * is returned.
+     * 
+     * Supports deep get using dotted format.
+     * 
+     * @param key 
+     * @param defaultValue 
+     */
+    public get(key: string, defaultValue: any = null): any {
+        return Obj.get(this._data, key, defaultValue);
+    }
+
+    /**
+     * Sets a new data on the session field.
+     * 
+     * @param key 
+     * @param value 
+     */
+    public set(key: string, value: any): ISession {
+        this._data[key] = value;
+
+        return this;
+    }
+
+    /**
+     * Returns the complete session data.
+     * 
+     * @returns object
+     */
+    public data(): AnyObject {
+        return this._data;
+    }
+
+}
